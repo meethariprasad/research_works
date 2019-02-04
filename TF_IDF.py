069f5a91072d8dfa701a4662c93270e8a7998433
meethariprasad <meethariprasad@gmail.com>
2019-02-04 14:08:52 +0530

    TF IDF Function

---
+++ TF_IDF.py
@@ -0,0 +1,69 @@
+#A simple file for TFIDF. I created this because, TFIDF of sklearn was not giving expected results by reducing TFIDF score for frequent 
+#words and increasing for rare words for some reason.
+#Usage: Input: List of text documents. Output: TFIDF Dataframe. Pre processing can be altered in doclist2_cleanlist
+
+def doclist2_cleanlist(document_list):
+    clean_doc_list=[]
+    for doc_id,doc in enumerate(document_list):
+        import nltk
+        sent_text = nltk.sent_tokenize(doc) # this gives us a list of sentences
+        clean_sent_list=[]
+        for sentence in sent_text:
+            sentence=sentence.lower()
+            import re
+            clean_sent=re.sub('[^A-Za-z0-9_-]+', ' ', sentence).strip()
+            clean_sent_list.append(clean_sent)
+        clean_doc_list.append(' '.join(clean_sent_list))
+    return(clean_doc_list)
+
+def tf_of_words_per_doc(documents):
+    list_of_unique_words_in_documents=list(set(' '.join(documents).split()))
+    doc_word_TF_Doc_word=[]
+    for word in list_of_unique_words_in_documents:
+        for doc_id,doc in enumerate(documents):
+            Number_of_words_in_current_doc=len(doc.split())
+            def count_if_found(str, word):
+                a = str.split(" ") 
+                count = 0
+                for i in range(0, len(a)): 
+                    if (word == a[i]): 
+                       count = count + 1 
+                return count 
+            number_of_time_word_repeated_in_current_doc=count_if_found(doc,word)
+            TF_Doc1_word=number_of_time_word_repeated_in_current_doc/Number_of_words_in_current_doc
+            doc_word_TF_Doc_word.append(list([doc_id,word,TF_Doc1_word]))
+    return(doc_word_TF_Doc_word)
+
+TF_Frame=pd.DataFrame(tf_of_words_per_doc(documents),columns=["doc","word","TF"])
+
+def idf_words(documents):
+    Number_of_documents=len(documents)
+    idf_words=[]
+    
+    def number_of_documents_word_appears(word,documents):
+        number_of_documents_word_appears_count=0        
+        for doc in documents:
+            unique_words_per_doc=(list(set(doc.split())))
+            for doc_word_set in unique_words_per_doc:
+                if word in doc_word_set:
+                    number_of_documents_word_appears_count=number_of_documents_word_appears_count+1
+        return(number_of_documents_word_appears_count)
+    
+    list_of_unique_words_in_documents=list(set(' '.join(documents).split()))
+    
+    for word in list_of_unique_words_in_documents:
+        number_of_documents_word_appears_count=number_of_documents_word_appears(word,documents)
+        IDF_Doc_word=math.log(Number_of_documents/(1+number_of_documents_word_appears_count))+1
+        idf_words.append(list([word,Number_of_documents,number_of_documents_word_appears_count,IDF_Doc_word]))
+
+    return(idf_words)
+
+IDF_Frame=pd.DataFrame(idf_words(documents),columns=["word","Number_of_documents","number_of_documents_word_appears_count","idf"])
+
+def get_tf_idf_frame(documents):
+    documents=doclist2_cleanlist(documents)
+    TF_Frame=pd.DataFrame(tf_of_words_per_doc(documents),columns=["doc","word","TF"])
+    IDF_Frame=pd.DataFrame(idf_words(documents),columns=["word","Number_of_documents","number_of_documents_word_appears_count","idf"])
+    TF_IDF_Master_Frame=TF_Frame.merge(IDF_Frame,how="left",left_on="word",right_on="word").sort_values("doc")
+    TF_IDF_Master_Frame["TF_IDF"]=TF_IDF_Master_Frame["TF"]*TF_IDF_Master_Frame["idf"]
+    return (TF_IDF_Master_Frame)
